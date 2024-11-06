631c9cd18e07aada10cb0bc7f1ef60d3c31f1537
dpajak99 <37120969+dpajak99@users.noreply.github.com>
2024-11-06 15:00:00 +0100

    Google Sheets integration: Introduce simple HTML parser

---
+++ test/test.dart
@@ -0,0 +1,19 @@
+// <span style="font-size:10pt;font-family:Arial;font-style:normal;" data-sheets-root="1">
+//   <span style="font-size:10pt;font-family:Arial;font-style:normal;">a</span>
+//   <span style="font-size:10pt;font-family:Arial;font-weight:bold;font-style:normal;">sd</span>
+//   <span style="font-size:10pt;font-family:Arial;font-style:normal;color:#ff0000;">fa</span>
+//   <span style="font-size:10pt;font-family:Arial;font-style:normal;">sf</span>
+//   <span style="font-size:14pt;font-family:Arial;font-style:normal;">as</span>
+// </span>
+
+import 'package:sheets/utils/html_span_parser.dart';
+
+void main() {
+  HtmlSpanParser htmlSpanParser = HtmlSpanParser();
+
+  String htmlString = '<style type="text/css"><!--td {border: 1px solid #cccccc;}br {mso-data-placement:same-cell;}--></style><span style="font-size:10pt;font-family:Arial;font-style:normal;" data-sheets-root="1"><span style="font-size:10pt;font-family:Arial;font-style:normal;">a</span><span style="font-size:10pt;font-family:Arial;font-weight:bold;font-style:normal;">sd</span><span style="font-size:10pt;font-family:Arial;font-style:normal;color:#ff0000;">fa</span><span style="font-size:10pt;font-family:Arial;font-style:normal;">sf</span><span style="font-size:14pt;font-family:Arial;font-style:normal;">as</span></span>';
+
+  SheetSpan customSpan = htmlSpanParser.parse(htmlString);
+
+  print(customSpan);
+}
